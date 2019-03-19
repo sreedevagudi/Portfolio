dcb075cb47e8a7866368dd0ae391a5671ae72073
Sreenath D <sreedevagudi@gmail.com>
2019-03-19 08:18:19 +0530

    new changes

--- views/aboutCntrl.js
+++ views/aboutCntrl.js
@@ -8,78 +8,71 @@
 angular.module('myResp')
     .controller("aboutController", ['$scope', function($scope){    
          $scope.skills = [
-        { "name": "Languages/Scripts",
-          "list": [
-                {"prof": "HTML5"},
-                {"prof": "CSS3"},
-                {"prof": "XSLT"},
-                {"prof": "JavaScript"} 
+          { "name": "Design",
+            "list": [
+                  {"prof": "Sketch"},
+                  {"prof": "Adobe Creative Suite (Photoshop, Illustrator)"},
+                  {"prof": "VISIO"},
+                  {"prof": "Proto"} 
+              ]
+          },
+          { "name": "Framework",
+            "list": [
+                  {"prof": "Twitter Bootstrap"},
+                  {"prof": "SCSS"},
+                  {"prof": "Angular JS"}
+              ]
+          },
+          { "name": "Pre-processor",
+            "list": [
+                  {"prof": "SCSS"}
+              ]
+          },
+          { "name": "Web Development",
+            "list": [
+                  {"prof": "HTML5"},
+                  {"prof": "CSS3"},
+                  {"prof": "Parallax"},
+                  {"prof": "RESTful API"},
+                  {"prof": "JQuery"},
+                  {"prof": "JavaScript"}              
+              ]
+          },
+          { "name": "Version Controller",
+            "list": [
+              {"prof": "GIT"},
+                {"prof": "Tortoise SVN"}
             ]
-        },
-        { "name": "Framework",
-          "list": [
-                {"prof": "Twitter Bootstrap"},
-                {"prof": "SCSS"},
-                {"prof": "Angular JS"}
-            ]
-        },
-        { "name": "Web Development",
-          "list": [
-                {"prof": "HTML5"},
-                {"prof": "CSS3"},
-                {"prof": "XML"},
-                {"prof": "XSLT"},
-                {"prof": "JQuery"}               
-            ]
-        },
-        { "name": "Operating Systems",
-          "list": [
-                {"prof": "Win XP, 7, 8 and 10"}
-            ]
-        },
-        { "name": "Testing",
-          "list": [
-              {"prof": "Unit Testing"}
-          ]
-        },
-        { "name": "Database",
-          "list": [
-              {"prof": "MySQL"}
-          ]
-        },
-        { "name": "Version Controller",
-          "list": [
-              {"prof": "Git"},
-              {"prof": "Tortoise SVN"}
-          ]
-        },
-        { "name": "Building/Testing Tools",
-          "list": [
-                {"prof": "Web Development Tool"},
-                {"prof": "Firebug"},
-                {"prof": "Color Picker"},
-                {"prof": "Page Ruler"}        
-            ]
-        },
-        { "name": "Tools",
-          "list": [
-                {"prof": "O365/SharePoint Online Designer"},
-                {"prof": "SharePoint Designer 2007 and 2010"},
-                {"prof": "Adobe Dreamweaver cs5.1"},
-                {"prof": "Adobe Photoshop cs5.1"},
-                {"prof": "Visual Studio 2010 and 2013"},
-                {"prof": "Microsoft's FrontPage"},
-                {"prof": "Logi XML studio"},
-                {"prof": "Notepad++"},
-                {"prof": "Adobe Dreamweaver"},
-                {"prof": "Eclipse"},
-                {"prof": "Oracle Web Logic"}
-            ]
-        },
-        { "name": "Prototypes",
-          "list": [
-                {"prof": "Microsoft Visio"},
-                {"prof": "Proto"}
-            ]
-        }];    
+          },
+          { "name": "Building/Testing Tools",
+            "list": [
+                  {"prof": "Web Development Tool"},
+                  {"prof": "Firebug"},
+                  {"prof": "Color Picker"},
+                  {"prof": "Page Ruler"}        
+              ]
+          },
+          { "name": "Testing",
+            "list": [
+                  {"prof": "Jasmine"},
+                  {"prof": "Browsera"},
+                  {"prof": "Functional Testing"},
+                  {"prof": "Regression Testing"},
+                  {"prof": "Integration Testing"},
+                  {"prof": "System Testing"},
+                  {"prof": "Deployment Testing"}
+              ]
+          },
+          { "name": "Tools",
+            "list": [
+                  {"prof": "Visual studio code"},
+                  {"prof": "Dreamweaver"},
+                  {"prof": "Visual Studio 20XX"},
+                  {"prof": "Logi XML studio"},
+                  {"prof": "Notepad++"},
+                  {"prof": "Eclipse"},
+                  {"prof": "Oracle Web Logic"}
+              ]
+          }
+  ];    
 }]);
